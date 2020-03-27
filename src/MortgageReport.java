7b7c8f3f3e0514587ed7f8151a7d3894af4f4103
Darian Glausier <darianglausier@gmail.com>
2020-03-27 09:33:12 -0400

    Extracting MortgageReport class.

---
+++ src/MortgageReport.java
@@ -0,0 +1,22 @@
+import java.text.NumberFormat;
+
+public class MortgageReport {
+    public static void printMortgage(int principal, double interest, int years){
+        double mortgage = Main.mortgage(principal, interest, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+    public static void printPaymentSchedule(int principal, double interest, int years){
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (int month = 1; month <= years * Main.monthsInYear; month++) {
+            double balance = Main.balance(principal, interest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+}
